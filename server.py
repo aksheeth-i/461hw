d8726610c5f8a2f6cb90c0c94cd35fce14888937
aksheeth-i <81332136+aksheeth-i@users.noreply.github.com>
2022-03-30 17:22:21 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 
-flask_app = Flask(__name__, static_folder='./frontend/build', static_url_path='/')
+flask_app = Flask(__name__, static_folder='./build', static_url_path='/')
 
 @flask_app.route('/name/Aksheeth')
 def lastName():
